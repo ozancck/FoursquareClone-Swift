d7ca6a4335c496bcd54d7f871b5342214a00a7e5
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-11-11 18:09:32 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Ozan Çiçek on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
